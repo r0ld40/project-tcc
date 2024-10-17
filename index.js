36323e6a395c8989a4e4ddb9035033000e7f852d
JoaoVitor <roldao0634@gmail.com>
2024-10-17 15:31:11 -0300

    v1.2

--- index.js
+++ index.js
@@ -1,58 +1,61 @@
-const array = async () => {
-    try {
-        const response = await fetch("./Livros.json");
-        if (!response.ok) {
-            throw new Error(`Erro ao buscar o JSON: ${response.statusText}`);
-        }
-        const data = await response.json(); // Converte a resposta para JSON
-        return data;
-    } catch (error) {
-        console.error("Erro ao carregar JSON:", error);
+const livros = [
+    {
+        "nome": "nome",
+        "autor": "autor",
+        "id": "1",
+        "genero": "1",
+        "preco": "1",
+        "capa": "./assets/image 1.png"
+    },
+    {
+        "nome": "nome 2",
+        "autor": "autor 2",
+        "id": "2",
+        "genero": "2",
+        "preco": "2",
+        "capa": "./assets/image 1.png"
+    },
+    {
+        "nome": "nome 3",
+        "autor": "autor 3",
+        "id": "3",
+        "genero": "3",
+        "preco": "3.45",
+        "capa": "./assets/image 1.png"
     }
-}; // Puxa a Lista de livros
+]; // Puxa a Lista de livros
 
 document.addEventListener('DOMContentLoaded', async () => {
     const divLivros = document.getElementById('divLivros');
 
     divLivros.style.display = "flex";
+    livros.forEach((livro) => {
+        const livroDiv = document.createElement('div');
+        livroDiv.style.border = '2px solid black';
+        livroDiv.style.height = 'auto';
+        livroDiv.style.display = 'flex';
+        livroDiv.style.flexDirection = 'column';
+        livroDiv.style.alignItems = 'start';
+        livroDiv.style.justifyContent = 'space-between';
 
-    try {
-        const livros = await array(); 
-        if (livros) {
-            livros.forEach((livro) => {
-                const livroDiv = document.createElement('div');
-                livroDiv.style.border = '2px solid black';
-                livroDiv.style.height = 'auto';
-                livroDiv.style.display = 'flex';
-                livroDiv.style.flexDirection = 'column';
-                livroDiv.style.alignItems = 'start';
-                livroDiv.style.justifyContent = 'space-between';
+        const img = document.createElement('img');
+        img.src = livro.capa;
+        img.alt = `Capa do livro ${livro.capa}`;
+        img.style.width = '190px';
+        img.style.height = '240px';
 
-                const img = document.createElement('img');
-                img.src = livro.capa;
-                img.alt = `Capa do livro ${livro.capa}`;
-                img.style.width = '190px';
-                img.style.height = '240px';
+        const infoDiv = document.createElement('div');
+        infoDiv.innerHTML = `
+            <h3>${livro.nome}</h3>
+            <p>Autor: ${livro.autor}</p>
+            <p>ID: ${livro.id}</p>
+            <p>Genero: ${livro.genero}</p>
+            <p>Preco: R$ ${livro.preco}</p>
+        `;
+        infoDiv.style.margin = "5px";
 
-                const infoDiv = document.createElement('div');
-                infoDiv.innerHTML = `
-                    <h3>${livro.nome}</h3>
-                    <p>Autor: ${livro.autor}</p>
-                    <p>ID: ${livro.id}</p>
-                    <p>Genero: ${livro.genero}</p>
-                    <p>Preco: R$ ${livro.preco}</p>
-                `;
-                infoDiv.style.margin = "5px";
-
-                livroDiv.appendChild(img);
-                livroDiv.appendChild(infoDiv);
-                divLivros.appendChild(livroDiv);
-            });
-        } else {
-            divLivros.innerText = 'Nenhum livro encontrado.';
-        }
-    } catch (error) {
-        divLivros.innerText = 'Erro ao carregar os livros.';
-        console.error(error);
-    }
+        livroDiv.appendChild(img);
+        livroDiv.appendChild(infoDiv);
+        divLivros.appendChild(livroDiv);
+    });
 }); // Carrega os Livros em "Livros mais populares"
